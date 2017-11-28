d2ee6a82ad9b5b854cb80a503a8a2e56cb0c4519
Matt Thomas <mmattthomas@gmail.com>
2017-11-28 10:00:38 -0800

    improvements towards single flow

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 var https = require('https');
-var fs = require('fs');
+const fs = require('fs');
 var cfg = require('./config');
 var moment = require('moment');
 var json2csv = require('json2csv');
 var dotenv = require('dotenv');
+var prompt = require('prompt');
+var parse = require('csv-parse');
+
+var promptChoiceSchema = {
+    properties: { 
+        choicename: {
+            description: 'Enter choice',
+            type: 'string',
+            required: true 
+        }
+    }
+}
+
+var promptKeySchema = {
+    properties: { 
+        keyname: {
+            description: 'Enter Key for table',
+            required: true 
+        }
+    }
+}
+
+var promptFileNameSchema = {
+    properties: { 
+        filename: {
+            description: 'Enter CSV filename',
+            type: 'string',
+            required: true 
+        },
+        tablename: {
+            description: 'Enter table name for source data',
+            type: 'string',
+            required: true
+        }
+    }
+}
 
 dotenv.load();
 console.log("SNOWREST2CSV v0.1 operational...");
@@ -11,35 +47,119 @@ console.log("SNOWREST2CSV v0.1 operational...");
 var csvFileName = "output.csv";
 var cmdKey = process.argv.slice(2)[0]; 
 if (!cmdKey) {
-    console.log("WARNINGERROR: you did not supply a key");
 
-} else {
+    console.log("Enter choice on how to create output:");
+    console.log("1: by filename - provide filename of CSV file with column headers");
+    console.log("2: by key - provide special key that configuration knows for table and columns");
+
+    prompt.message = "";
+    prompt.start();
+
+    prompt.get(promptChoiceSchema, function(err, result) {
+        if (result.choicename == "2") {
+            prompt.get(promptKeySchema, function(err, result) {
+                console.log("processing for key:" + result.keyname + "...");
+                doKeyRequest(result.keyname);
+            });            
+        } else {
+            console.log("Provide CSV file with following format");
+            console.log("First Row: column headers to display on output file");
+            console.log("Second Row: columns to request from REST API");
+            console.log("Third row (optional): queryparam string in ServiceNow format (i.e.:u_work_unit%3DDigital%20Insight%5Eu_account_type%3Dcustomer)");
+            prompt.get(promptFileNameSchema, function(err, result) {
+                doFileRequest(result.filename);
+                console.log("processing file:" + result.filename + "...");
+            });
+        }
+    })
+    
+
+} else {    
+    //TODO: parse cmdKey, 
+    // if endswith .csv, then its not a key but csv file
+    // read that csv file, use the part before file extension as table_name
+    // so customer_account.csv would:
+    //      read that file, capture every column name into array of columns
+    //      create select on 'customer_account' table
+    //      dump results to 'customer_account_output.csv' file
+    // print out where the results are.
+    if (cmdKey.endsWith('.csv')) {
+        doFileRequest(cmdKey);
+    } else {
+        doKeyRequest(cmdKey);    
+    }
+}
+
+function doKeyRequest(withKey) {
     var restCalls = cfg.calls;
     var restCallOptions = restCalls.filter(function(item) {
-        return item.key == cmdKey;
+        return item.key == withKey;
     });
     if (restCallOptions.length > 0) {
         doRequest(restCallOptions[0]);
     } else {
-        console.log("Unable to find any REST calls for " + cmdKey);
+        console.log("Unable to find any REST calls for " + withKey);
     }
+}
+
+// -- 
+function doFileRequest(withFileName) {
+    
+    var fileNameParts = withFileName.split('.');
+    var table_name = fileNameParts[0];
+
+    var restCallOptions = {
+        key: table_name,
+        table: table_name,
+        //query: "u_work_unit%3DDigital%20Insight%5Eu_account_type%3Dcustomer",
+        //fields: ["u_customer_type", "u_tax_id", "u_fdic_number", "u_potential_end_users", "u_tier", "u_master_customer_number", "name", "u_active", "u_short_name", "u_fi_type", "u_ncua_id", "u_routing_transit_number", "u_di_number"],
+        headerFields: "",
+        restFields: "",
+        limit: 200
+    };
+
+    // // todo: parse the CSV file
+    fs.readFile(withFileName, function(err, fileData) {
+        parse(fileData, {trim: true, to: 3}, function(err, rows) {
+            restCallOptions.headerFields = '"' + rows[0].join(',') + '"';
+            restCallOptions.restFields =  '"' + rows[1].join(',') + '"';
+            if (rows.length > 2) {
+                restCallOptions.query = rows[2][0];
+            }
+
+            doRequest(restCallOptions);
+        });
+    });
+    
     
 }
 
 function doRequest(restCallOptions) {
     
     // build field array from options
-    var snFields = restCallOptions.fields.split(',');
+    var headerFields = restCallOptions.headerFields.split(',');
+    var snFields = restCallOptions.restFields.split(',');
 
-    // build query string param path
-    var restPath = "/api/now/table/" + restCallOptions.table;
-    if (restCallOptions.query) {
-        restPath += "?sysparm_query=" + restCallOptions.query;
+    if (headerFields.length !== snFields.length) {
+        console.log("List of Header fields does not match list of fields");
+        console.log('dbg - headerfieldcount=' + headerFields.length);
+        console.log('dbg - detailfieldcount=' + snFields.length);
+        return;
     }
-    restPath += "&sysparm_fields=" + snFields.join("%2C");
+    
+    // build query string param path
+    var restPath = "/api/now/table/" + restCallOptions.table + '?';
+    if (restCallOptions.query) {
+        restPath += "sysparm_query=" + restCallOptions.query + '&';
+    }
+    restPath += "sysparm_fields=" + snFields.join("%2C");
     if (restCallOptions.limit) {
         restPath += "&sysparm_limit=" + restCallOptions.limit;
     }
+    restPath += "&sysparm_exclude_reference_link=TRUE"
+
+    //console.log('rest option query:' + restCallOptions.query);
+    //console.log('rest uri:' + restPath);
 
     // build options
     var options = {
@@ -64,10 +184,12 @@ function doRequest(restCallOptions) {
             try {
                 
                 snData = JSON.parse(rawData);
-                var csvResult = json2csv({ data: snData.result, fields: snFields });                
+                var csvResult = '';
+                //csvResult += '"' + headerFields.join('","') + '"\r\n';
+                csvResult += json2csv({ data: snData.result, fields: snFields, fieldNames: headerFields });                
                 console.log(csvResult);
 
-                writeCsvFile(csvResult, csvFileName);
+                writeCsvFile(csvResult, restCallOptions.key+'_out.csv');
 
                 // var output = writeCsv(snData.result, fields);
                 // console.log(output);
--- config.js
+++ config.js
@@ -9,17 +9,45 @@ var config = {
         {
             key: "customer_account",
             table: "customer_account",
-            query: "work_unit%3Ddigital%20insight",
+            query: "u_work_unit%3DDigital%20Insight%5Eu_account_type%3Dcustomer",
             //fields: ["u_customer_type", "u_tax_id", "u_fdic_number", "u_potential_end_users", "u_tier", "u_master_customer_number", "name", "u_active", "u_short_name", "u_fi_type", "u_ncua_id", "u_routing_transit_number", "u_di_number"],
-            fields: "u_customer_type,u_tax_id,u_fdic_number,u_potential_end_users,u_tier,u_master_customer_number,name,u_active,u_short_name,u_fi_type,u_ncua_id,u_routing_transit_number,u_di_number",
-            limit: 10
+            headerFields: "COMPANY_ID,RN_CREATE_DATE,RN_CREATE_USER,RN_EDIT_DATE,RN_EDIT_USER,COMPANY_NAME,ADDRESS_1,ZIP,PHONE,FAX,WWW,PARENT_COMPANY_ID,STATE_,CITY,TYPE,RSM_OVERRIDE,DUNS,AM_ID,REGULATORY_AGENCY,FI,DI_NUMBER,IB_REGISTRATION_TYPE,SHORT_NAME,BUSINESS_HOURS,AFTER_HOURS,BILL_PAYMENT_SPONSOR_ID,NO_OF_FI_CUSTOMERS,ASSET_SIZE,ROUTING_TRANSIT_NUMBER,ACTIVE,TAX_ID,MAILING_ADDRESS_LINE_ONE,MAILING_ADDRESS_CITY,MAILING_ADDRESS_STATE,MAILING_ADDRESS_ZIP,TIER,ACTIVE_STATUS,FDIC_NUMBER,INTERFACE_SUPPORT_TYPE,MONITORED_DOMAIN,LOADED_IN_SKYMON,SUPPORT_SERVICE_LEVEL,OMIT_MONITORING,CUSTOMER_CLASSIFICATION,ACTUAL_DEACTIVATION_DATE,PLANNED_DEACTIVATION_DATE,DPV_ID,DPV_PRODUCT_ID,DPV_SERVICE_BUREAU_ID,DPV_HOSTING_TYPE,BILL_PAYMENT_CM_SPONSOR_ID,BILLING_CLOSED_DATE,PREPROD_LOADED_IN_SKYMON,PREPROD_OMIT_MONITORING,BILL_PAY_COST_METHOD,IB_STAGING_URL,TEMP_BP_SPONSOR_ID,STAGING_SKYMON_STATUS,COMBINED_REGISTRATION,EMAIL_COLLECTION,EMAIL_CAPTURE,SRT,ALERTS,RETENTION_EMAILS,LENDING_SALES_REP,MONITORED_DOMAIN_OWNER,OL_INTERFACE_SERVICE_NAME,MYDI_EXCLUDE_FROM_NEW_INCIDENT,OMIT_MONITORING_REASON,PREPROD_OMIT_REASON,ACTIVE_DPV_ID,NCUA_ID,CEU,CUSTOMER_CENTRAL_ID,COMPANY_TYPE_F,ONLINE_CHAT_ENABLED,ONLINE_KB_ENABLED,COMPANY_CLASSIFICATION",
+            restFields: "sys_id,sys_created_on,sys_created_by,sys_updated_on,sys_updated_by,name,street,zip,phone,fax_phone,website,parent,state,city,u_customer_type,,u_duns,u_relationship_manager,,u_fi_type,u_di_number,u_ib_registration_type,u_short_name,u_business_hrs,u_after_hrs,u_bp_sponsor_ib,u_potential_end_users,u_asset_size,u_routing_transit_number,u_active,u_tax_id,u_alt_street,u_alt_city,u_alt_state,u_alt_zip,u_tier,u_active,u_fdic_number,u_interface_support_type,u_monitoring_domain,u_loaded_skymon,u_support_service_level,u_omit_monitoring,u_customer_classification,u_actual_deactivation_date,u_planned_deactivation_date,u_dpv,u_dpv_product,u_dpv_service_bureau,u_dpv_hosting_type,u_bp_sponsor_bt,u_billing_closed_date,u_preprod_loaded_skymon,u_preprod_omit_monitoring,u_bill_pay_cost_method,u_ib_staging_url,u_bp_sponsor_bfs,u_staging_status,u_combined_registration,u_email_collection,u_email_capture,u_srt,u_alerts,u_retention_emails,,u_monitored_domain_owner,u_ol_interface_service_name,,u_omit_monitoring_reason,u_preprod_omit_reason,,u_ncua_id,u_competitive_end_users,u_customer_central_id,,u_online_chat_enabled,u_online_kb_enabled,u_company_classification",
+            limit: 20
         },
+        {
+            
+            key: "customer_account_lite",
+            table: "customer_account",
+            query: "u_work_unit%3DDigital%20Insight%5Eu_account_type%3Dcustomer",
+            //fields: ["u_customer_type", "u_tax_id", "u_fdic_number", "u_potential_end_users", "u_tier", "u_master_customer_number", "name", "u_active", "u_short_name", "u_fi_type", "u_ncua_id", "u_routing_transit_number", "u_di_number"],
+            headerFields: "Type,Tax_ID,FDIC_Number,No_of_FI_Customers,Tier,NCR_Master_Customer_Number,Company_Name,Active,Short_Name,FI,NCUA_ID,Routing_Transit_Number,DI_Number",
+            restFields: "u_customer_type,u_tax_id,u_fdic_number,u_potential_end_users,u_tier,u_master_customer_number,name,u_active,u_short_name,u_fi_type,u_ncua_id,u_routing_transit_number,u_di_number",
+            limit: 20
+        },
+        {
+            key: "product_lite",
+            table: "u_cmdb_ci_di_product",
+            queryparams: "u_active%3Dtrue",
+            headerFields: "Code_",
+            restFields: "u_product_code",
+            limit: 20
+        }, 
         {
             key: "product",
             table: "u_cmdb_ci_di_product",
             queryparams: "",
-            fields: "full_name,u_supportable,u_active,u_product_code,type",
-            limit: 10
+            headerFields: "Product_Id,Rn_Descriptor,Rn_Create_Date,Rn_Create_User,Rn_Edit_Date,Rn_Edit_User,Product_Name,Code_,Type,Product_Manager_Id,Product_Class_Id,Product_Number,Product_Category_Id,Sellable,Supportable,MyDI_Early_Support",
+            restFields: "sys_id,display_name,sys_created_on,sys_created_by,sys_updated_on,sys_updated_by,name,u_product_code,type,owner,u_implementation_class_id,model_number,cmdb_model_category,u_sellable,u_supportable,u_extranet_early_support",
+            limit: 20
+        }, 
+        {
+            key: "asset",
+            table: "",
+            queryparams: "",
+            headerFields: "",
+            restFields: "",
+            limit: 0
         }
     ]
 
